18528b8c5eb4103261edaf49b22dda35994c5739
Jamie Hunt <jamie@condorwebworks.co.uk>
2023-12-05 00:17:28 +0000

    s

--- src/components/AboutUsCards.jsx
+++ src/components/AboutUsCards.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-
 import Image from "next/image";
 
 // Correct import
 import driver from "../../public/images/driver.jpg";
 import globe from "../../public/images/globe.jpg";
-import drivinginstructor from "../../public/images/drivinginstructor.jpg";
+import drivinginstructor from "../../public/images/drivingInstructor.jpg";
 
 const iconVariant = [driver, globe, drivinginstructor];
 
 export function AboutUsCards({ title, descriptions, iconIndex }) {
   return (
-    <div className="relative flex h-72 min-h-[10rem] w-96  shrink-0 grow-0 flex-col gap-2 rounded-lg shadow-md shadow-slate-500">
+    <div className="relative flex h-72 min-h-[10rem] w-96 shrink-0 grow-0 flex-col gap-2 rounded-lg shadow-md shadow-slate-500">
       <Image
         src={iconVariant[iconIndex]}
         className="absolute left-0 top-0 h-full shrink-0 rounded-lg object-cover"
       />
 
-      <div className=" absolute left-0 top-0 flex h-full w-full shrink-0 flex-col justify-evenly rounded-lg bg-gray-500/60	text-white	backdrop-blur-sm">
+      <div className="absolute left-0 top-0 flex h-full w-full shrink-0 flex-col justify-evenly rounded-lg bg-gray-500/60 text-white backdrop-blur-sm">
         <h4 className="mt-16 w-full text-center text-xl font-bold drop-shadow-lg">
           {title}
         </h4>
-        <p className=" mb-0 mt-auto flex h-1/2 items-start justify-start rounded-b-lg bg-black/60 px-4 pt-4 text-justify text-sm backdrop-blur-lg">
+        <p className="mb-0 mt-auto flex h-1/2 items-start justify-start rounded-b-lg bg-black/60 px-4 pt-4 text-justify text-sm backdrop-blur-lg">
           {descriptions}
         </p>
       </div>
